c22dd71fe29e1973090dcba10ce31e9decd3198f
BJR Matos <bjrmatos@gmail.com>
2018-04-19 15:00:05 -0500

    add optionsSchema

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,5 +1,30 @@
+
 module.exports = {
   'name': 'chrome-pdf',
   'main': 'lib/chrome.js',
+  'optionsSchema': {
+    extensions: {
+      'chrome-pdf': {
+        type: 'object',
+        properties: {
+          timeout: { type: 'number' },
+          launchOptions: {
+            type: 'object',
+            properties: {
+              args: {
+                anyOf: [{
+                  type: 'string',
+                  '$jsreport-constantOrArray': []
+                }, {
+                  type: 'array',
+                  items: { type: 'string' }
+                }]
+              }
+            }
+          }
+        }
+      }
+    }
+  },
   'dependencies': [ 'templates' ]
 }
