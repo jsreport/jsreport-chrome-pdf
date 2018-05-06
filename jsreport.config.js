b4fcbc21f1f7fc7b895e611d9fb434c7b2ef59e5
BJR Matos <bjrmatos@gmail.com>
2018-05-06 13:26:24 -0500

    add top level “chrome” option to schema

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,29 +1,32 @@
 
+const chromeSchema = {
+  type: 'object',
+  properties: {
+    timeout: { type: 'number' },
+    launchOptions: {
+      type: 'object',
+      properties: {
+        args: {
+          anyOf: [{
+            type: 'string',
+            '$jsreport-constantOrArray': []
+          }, {
+            type: 'array',
+            items: { type: 'string' }
+          }]
+        }
+      }
+    }
+  }
+}
+
 module.exports = {
   'name': 'chrome-pdf',
   'main': 'lib/chrome.js',
   'optionsSchema': {
+    chrome: { ...chromeSchema },
     extensions: {
-      'chrome-pdf': {
-        type: 'object',
-        properties: {
-          timeout: { type: 'number' },
-          launchOptions: {
-            type: 'object',
-            properties: {
-              args: {
-                anyOf: [{
-                  type: 'string',
-                  '$jsreport-constantOrArray': []
-                }, {
-                  type: 'array',
-                  items: { type: 'string' }
-                }]
-              }
-            }
-          }
-        }
-      }
+      'chrome-pdf': { ...chromeSchema }
     }
   },
   'dependencies': [ 'templates' ]
